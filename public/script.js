23d53571e8380f34dceb58c71e62eb07f1b5109f
Marcelino Llano <hello@marcelinollano.com>
2019-04-25 00:32:32 +0200

    Try jumping javascript to position

--- public/script.js
+++ public/script.js
@@ -139,7 +139,7 @@ document.addEventListener(
       event.target.innerHTML = "Play";
       return;
     }
-    event.target.audio.currentTime = 5;
+    event.target.audio.currentTime = 6;
     event.target.audio.play();
     event.target.setAttribute("aria-pressed", "true");
     event.target.innerHTML = "Pause";
